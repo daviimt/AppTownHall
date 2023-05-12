8c43f791e88aadb7666c8f876bff85337a6b8ba1
David <dmateomerino@gmail.com>
2023-05-12 11:12:13 +0200

    interface

--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -29,7 +29,8 @@ class _HeaderIcon extends StatelessWidget {
       child: Container(
         width: double.infinity,
         margin: const EdgeInsets.only(top: 30),
-        child: const Icon(Icons.person_pin, color: Colors.white, size: 100),
+        child: const Icon(Icons.account_balance_rounded,
+            color: Colors.white, size: 100),
       ),
     );
   }
